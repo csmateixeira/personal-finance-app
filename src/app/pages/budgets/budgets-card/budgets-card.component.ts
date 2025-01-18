b2fa00c30285d486c39ac433f08a5782b777c75e
Carla T <csmateixeira@users.noreply.github.com>
2025-01-18 22:20:56 +0000

    Add "Delete Budget" menu option and implement action to resolve #31

--- src/app/pages/budgets/budgets-card/budgets-card.component.ts
+++ src/app/pages/budgets/budgets-card/budgets-card.component.ts
@@ -1,39 +1,59 @@
 import {Component, inject, Input} from '@angular/core';
 import {AsyncPipe, CurrencyPipe, DatePipe, NgOptimizedImage, NgStyle, PercentPipe} from '@angular/common';
-import {Budget, BudgetSpending, Transaction} from '../../../../utils/models';
+import {Budget, BudgetSpending, Spending, Transaction} from '../../../../utils/models';
 import {Store} from '@ngrx/store';
 import {SidebarState} from '../../../sidebar/state/sidebar.state';
-import {Observable} from 'rxjs';
+import {map, Observable} from 'rxjs';
 import {selectTransactionsData, selectTransactionsSpendings} from '../../transactions/state/transactions.state';
-import {BudgetsService} from '../../../../services/budgets.service';
+import {BudgetsUtils} from "../../../../utils/budgets-utils";
+import {TransactionsUtils} from "../../../../utils/transactions-utils";
 
 @Component({
-  selector: 'app-budgets-card',
-  imports: [
-    NgOptimizedImage,
-    NgStyle,
-    CurrencyPipe,
-    AsyncPipe,
-    PercentPipe,
-    DatePipe,
-  ],
-  templateUrl: './budgets-card.component.html',
-  styleUrl: './budgets-card.component.scss'
+    selector: 'app-budgets-card',
+    imports: [
+        NgOptimizedImage,
+        NgStyle,
+        CurrencyPipe,
+        AsyncPipe,
+        PercentPipe,
+        DatePipe,
+    ],
+    templateUrl: './budgets-card.component.html',
+    styleUrl: './budgets-card.component.scss'
 })
 export class BudgetsCardComponent {
-  private store: Store = inject(Store<{ sidebar: SidebarState }>);
-  private budgetsService: BudgetsService = inject(BudgetsService);
+    private store: Store = inject(Store<{ sidebar: SidebarState }>);
 
-  @Input() budget!: Budget;
+    @Input() budget!: Budget;
 
-  transactions$: Observable<Transaction[]> = this.getLatestTransactions();
-  budgetSpending$: Observable<BudgetSpending> = this.getBudgetSpending();
+    transactions$: Observable<Transaction[]> = this.getLatestTransactions();
+    budgetSpending$: Observable<BudgetSpending> = this.getBudgetSpending();
 
-  getBudgetSpending(): Observable<BudgetSpending> {
-    return this.budgetsService.getBudgetSpending(this.store.select(selectTransactionsSpendings), this.budget);
-  }
+    showDropdown = false;
 
-  getLatestTransactions(): Observable<Transaction[]> {
-    return this.budgetsService.getLatestTransactions(this.store.select(selectTransactionsData), this.budget.category);
-  }
+    toggleDropdown() {
+        this.showDropdown = !this.showDropdown;
+    }
+
+    getBudgetSpending(): Observable<BudgetSpending> {
+        return this.store.select(selectTransactionsSpendings).pipe(
+            map((spendings: Spending[]) => BudgetsUtils.findSpendingForCategory(spendings, this.budget.category)),
+            map((spending: Spending): BudgetSpending => BudgetsUtils.getBudgetSpending(this.budget, spending))
+        );
+    }
+
+    getLatestTransactions(): Observable<Transaction[]> {
+        return this.store.select(selectTransactionsData).pipe(
+            map((transactions: Transaction[]): Transaction[] => TransactionsUtils.filterTransactions(transactions, this.budget.category)),
+            map((transactions: Transaction[]): Transaction[] => transactions.slice(0, 3))
+        );
+    }
+
+    edit() {
+
+    }
+
+    delete() {
+
+    }
 }
--- src/services/budgets.service.ts
+++ src/services/budgets.service.ts
@@ -1,8 +1,7 @@
 import {Injectable} from '@angular/core';
-import {Budget, BudgetSpending, Spending, Transaction} from '../utils/models';
-import {map, Observable, of} from 'rxjs';
+import {Budget} from '../utils/models';
+import {Observable, of} from 'rxjs';
 import {Data} from '../utils/data';
-import {Utils} from '../utils/utils';
 
 @Injectable({
   providedIn: 'root'
@@ -10,25 +9,5 @@ import {Utils} from '../utils/utils';
 export class BudgetsService {
   getAllBudgets(): Observable<Budget[]> {
     return of(Data.getBudgets());
-  }
-
-  getBudgetSpending(spendings$: Observable<Spending[]>, budget: Budget): Observable<BudgetSpending> {
-    return spendings$.pipe(
-      map((spendings: Spending[]) =>
-        spendings.find((spending: Spending) => spending.category === budget.category) ?? { amount: 0 } as Spending),
-      map((spending: Spending): BudgetSpending => ({
-        ...budget,
-        spent: spending.amount,
-        percent: spending.amount > 0 ? spending.amount/budget.maximum : 0,
-        remaining: budget.maximum - spending.amount > 0 ? Utils.roundNumber(budget.maximum - spending.amount) : 0
-      }))
-    );
-  }
-
-  getLatestTransactions(transactions$: Observable<Transaction[]>, category: string): Observable<Transaction[]> {
-    return transactions$.pipe(
-      map((transactions: Transaction[]): Transaction[] => transactions.filter((transaction: Transaction) => transaction.category === category)),
-      map((transactions: Transaction[]): Transaction[] => transactions.slice(0, 3))
-    );
   }
 }
