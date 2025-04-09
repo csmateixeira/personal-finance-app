bddfbbf4490c081fdabd874a85ec476fe62d37de
Carla T <csmateixeira@users.noreply.github.com>
2025-04-09 18:03:45 +0100

    Added API call to get list of transactions to fix #64 (#65)

--- src/app/transactions/services/transactions.service.spec.ts
+++ src/app/transactions/services/transactions.service.spec.ts
@@ -1,20 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { TransactionsService } from './transactions.service';
+import {TransactionsService} from './transactions.service';
 import {cold} from 'jasmine-marbles';
-import {Data} from '../../shared/utils/data';
+import {HttpService} from '../../shared/services/http.service';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {of} from 'rxjs';
+import {TransactionsTestUtils} from '../../shared/utils/test-utils';
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
+  let httpService: HttpService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        HttpService,
+        HttpClient,
+        HttpHandler
+      ]
+    });
+
     service = TestBed.inject(TransactionsService);
+    httpService = TestBed.inject(HttpService);
   });
 
   it('should return all transactions', () => {
+    spyOn(httpService, 'doGet').and.returnValue(of(TransactionsTestUtils.getTransactions()));
+
     const expected = cold('(a|)', {
-      a: Data.getTransactions()
+      a: TransactionsTestUtils.getTransactions()
     })
 
     expect(service.getAllTransactions()).toBeObservable(expected);
